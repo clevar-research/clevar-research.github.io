416d4878bd3ddee19ea83c235ddff296f7dba1a3
joelaharman <34673280+joelaharman@users.noreply.github.com>
2025-08-17 01:37:15 +1000

    Initial Upload

---
+++ juicing/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "QUT CLEVAR Group-CLEVAR Study - Game Juice-0.1";
+const contentToCache = [
+    "Build/juicing.loader.js",
+    "Build/juicing.framework.js.br",
+    "Build/juicing.data.br",
+    "Build/juicing.wasm.br",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
